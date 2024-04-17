5820833a4cec46c4045c907172152043851998a2
muallayuce <muallayuce@gmail.com>
2024-04-17 16:50:02 +0200

    pull please

--- src/components/Marketplace.jsx
+++ src/components/Marketplace.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import './Marketplace.css';
 
-function Marketplace({ products }) { 
+function Marketplace({ products }) {
   return (
     <div className="marketplace">
-      {products.map(product => ( // Change 'product.id' to 'products.map(product => ...'
+      {products.map((product) => (
         <div key={product.id} className="product">
           <div className="product_header">
             <div>
               <p className="product_name">{product.product_name}</p>
-              <p className="product_price">{product.price}</p>
+              <p className="product_price">${product.price}</p>
             </div>
           </div>
           <p className="product_description">{product.description}</p>
-          {/* {product.id && (
-            <img className="product_image" src={`http://localhost:8000/images/${product.id}`} alt="Product Image" /> 
-          )} */}
+          {product.id && (
+            <img
+              className="product_image"
+              src={`http://localhost:8000/productimages/${product.id}`}
+              alt="Product Image"
+            />
+          )}
+          <p>Quantity: {product.quantity}</p>
+          <p>Username: {product.user.username}</p>
         </div>
       ))}
     </div>
@@ -23,3 +29,4 @@ function Marketplace({ products }) {
 }
 
 export default Marketplace;
+
