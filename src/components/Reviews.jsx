946dd3fefa61992b8bf8fb651e218bf2a490ba33
Cesar Gonzalez <cegogo_6@hotmail.com>
2024-04-22 22:01:44 +0200

    user ratings

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import './Reviews.css'
 
 function Reviews({ productId }) {
@@ -10,7 +12,7 @@ function Reviews({ productId }) {
                 const response = await fetch(`http://localhost:8000/products/${productId}/reviews`);
                 if (response.ok) {
                     const data = await response.json();
-                    setReviews(data); // No es necesario acceder a data.reviews
+                    setReviews(data);
                 } else {
                     throw new Error('Failed to fetch reviews');
                 }
@@ -22,14 +24,27 @@ function Reviews({ productId }) {
         fetchReviews();
     }, [productId]);
 
+    const renderStars = (score) => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            if (i <= score) {
+                stars.push(<FontAwesomeIcon icon={faStar} key={i} style={{ opacity: 1 }} />);
+            } else {
+                stars.push(<FontAwesomeIcon icon={faStar} key={i} style={{ opacity: 0.3 }} />);
+            }
+        }
+        return stars;
+
+    };
+
     return (
         <div className="reviews-container">
             <h3 className='reviews-title'>Reviews</h3>
             {reviews.map(review => (
                 <div key={review.creator_id} className="review-display">
-                    <p className='revirew-username'><strong>User:</strong> {review.creator_username.username}</p>
-                    <p><strong className='review-rating'>Rating:</strong> {review.score}</p>
-                    <p><strong className='review-comment'>Comment:</strong> {review.comment}</p>
+                    <p className='review-username'> {review.creator_username.username}</p>
+                    <p className='review-rating'> {renderStars(review.score)}</p>
+                    <p className='review-comment'> {review.comment}</p>
                 </div>
             ))}
         </div>
