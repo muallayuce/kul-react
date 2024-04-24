97f2f4f1c0e537fac5cfb5d69154b52d1e4a883c
muallayuce <muallayuce@gmail.com>
2024-04-24 16:49:10 +0200

    footer-login

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import balam from './assets/balam.png'
 import Logout from './components/Logout';
 import Home from './components/Home';
 import { UserContext } from './context/UserContext';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import ProductDetail from './components/ProductDetail';
 import UserProfile from './components/UserProfile/UserProfile';
 import Chat from './components/Chat';
@@ -32,6 +32,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [currentScreen, setCurrentScreen] = useState(null);
   const [showUserProfile, setShowUserProfile] = useState(false); // State to manage whether to display UserProfile component
+  const [footerScreen, setFooterScreen] = useState(null);
 
   // useEffect to check if user is logged in and fetch posts and products
   useEffect(() => {
@@ -85,6 +86,7 @@ function App() {
     await fetchPosts(); // Fetch posts after login
     setIsLoggedIn(true); // Update isLoggedIn state
     setOpenModal(null); // Close the login modal
+    setFooterScreen(null);
   };
 
   // Function to handle signup
@@ -93,6 +95,7 @@ function App() {
     console.log('Signing up with:', email, password);
     // After successful signup, you can close the modal
     setOpenModal(null);
+    setFooterScreen(null);
   };
 
   // Function to handle logout
@@ -113,6 +116,19 @@ function App() {
     setCurrentScreen('profile'); // Set currentScreen state to 'profile'
   };
 
+  const handlePrivacy = () => {
+    setFooterScreen('privacy')
+  }
+
+  const handleTerms = () => {
+    setFooterScreen('terms')
+  }
+
+  const handleContactUs = () => {
+    setFooterScreen('contactus')
+  }
+
+  
   return (
     <Router>
       <div className='app'>
@@ -136,20 +152,18 @@ function App() {
 
                 <div className='app_header_right'>
                   <Link to='/chat'> <i className="bi bi-chat-fill" id='chat_button'></i></Link> 
-                  <Link to='profile'><button className='profile_button' onClick={handleProfile}>Profile</button></Link>
+                  <Link to='/profile'><button className='profile_button' onClick={handleProfile}>Profile</button></Link>
                   <Link to="/"> <button className="logout_button" onClick={handleLogout}>Log Out </button></Link>
-                  
-                  <Link></Link>
-                  <Link></Link>
                 </div>
               </>
             ) : (
               <>
-                <div className='app_header_right'>
-                  <button className='register' onClick={() => setOpenModal('login')}>Log In</button>
-                  <button className='register' onClick={() => setOpenModal('signup')}>Sign Up</button>
+              <div className='app_header_right'>
+                <Link to="/login"><button className='register'>Log In</button></Link>
+                <Link to="/signup"><button className='register'>Sign Up</button></Link>
                 </div>
               </>
+
             )}
 
           </header>
@@ -176,6 +190,9 @@ function App() {
               ) : null
             }
           />
+          <Route path='/login' element={isLoggedIn ? <Navigate to="/posts" /> : <Login onLogin={handleLogin} />} />
+          <Route path='/signup' element={<Signup onSignup={handleSignup} />} />
+
           <Route path="/marketplace" element={<Marketplace products={products} />} />
           <Route path="/product/:productId" element={<ProductDetail />} />
           <Route path="/profile" element={<UserProfile/>} />
@@ -193,9 +210,9 @@ function App() {
         <footer className='footer'>
           <nav>
             <img className='footerImgLeft' src={footerImg} alt="" />
-            <Link to="/privacy">Privacy Policy</Link>
-            <Link to="/terms">Terms of Service</Link>
-            <Link to="/contactus">Contact Us</Link>
+            <Link to="/privacy" onClick={() => setFooterScreen('privacy')}>Privacy Policy</Link>
+            <Link to="/terms" onClick={() => setFooterScreen('terms')}>Terms of Service</Link>
+            <Link to="/contactus" onClick={() => setFooterScreen('contactus')}>Contact Us</Link>
             <img className='footerImgRight' src={balamw} alt="" />
           </nav>
           <p className='copy'>&copy; 2024 KUL-BALAM. All rights reserved.</p>
