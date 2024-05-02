1f74d31dbe2a1f7c3226b2e49b53eac56b1465a4
muallayuce <muallayuce@gmail.com>
2024-05-02 12:48:57 +0200

    creatgroup

--- src/components/UserProfile/Groups.jsx
+++ src/components/UserProfile/Groups.jsx
@@ -1,17 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import './UserProfile.css';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  TextField
+} from '@mui/material';
 
 const BASE_URL = 'http://localhost:8000';
 
 const Groups = () => {
   const [groups, setGroups] = useState([]);
+  const [open, setOpen] = useState(false);
+  const userId = localStorage.getItem('user_id');
+  const [newGroupData, setNewGroupData] = useState({
+    name: '',
+    description: '',
+    creator_id: userId ? parseInt(userId) : 0,
+    created_at: new Date().toISOString()
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewGroupData({ ...newGroupData, [name]: value });
+  };
+
+  const handleCreateGroup = async () => {
+    if (!newGroupData.name || !newGroupData.description) {
+      alert('Please provide both group name and description.');
+      return;
+    }
+
+    try {
+      const response = await fetch(`${BASE_URL}/groups/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newGroupData)
+      });
+      if (response.ok) {
+        fetchGroups();
+        setNewGroupData({
+          name: '',
+          description: '',
+          creator_id: 0,
+          created_at: new Date().toISOString()
+        });
+        handleClose();
+      } else {
+        throw new Error('Failed to create group');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Failed to create group');
+    }
+  };
 
   const fetchGroups = async () => {
     try {
-      const response = await fetch(BASE_URL + '/groups/all');
+      const response = await fetch(`${BASE_URL}/groups/all`);
       if (response.ok) {
         const groupsData = await response.json();
-        console.log('Groups:', groupsData);
         const updatedGroups = await Promise.all(
           groupsData.map(async (group) => {
             const members = await fetchGroupMembers(group.id);
@@ -22,7 +73,8 @@ const Groups = () => {
                 return { ...post, username };
               })
             );
-            return { ...group, members, posts: postsWithUsername };
+            const creatorUsername = await fetchUsername(group.creator_id);
+            return { ...group, members, posts: postsWithUsername, creatorUsername };
           })
         );
         setGroups(updatedGroups);
@@ -82,7 +134,7 @@ const Groups = () => {
 
   useEffect(() => {
     fetchGroups();
-  }, []); // Run this effect only once, when the component mounts
+  }, []);
 
   const toggleMemberList = (group) => {
     const updatedGroups = groups.map((grp) => {
@@ -94,24 +146,62 @@ const Groups = () => {
     setGroups(updatedGroups);
   };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <div className="groups">
-      <button className='create_groups'>Create Groups</button>
+      <Button variant="outlined" onClick={handleOpen}>
+        Create Group
+      </Button>
+      <Dialog open={open} onClose={handleClose} className="modal-container">
+        <DialogTitle className="modal-title">Create Group</DialogTitle>
+        <TextField
+          id="name"
+          name="name"
+          label="Group Name"
+          type="text"
+          value={newGroupData.name}
+          onChange={handleInputChange}
+          className="modal-input"
+        />
+        <TextField
+          margin="dense"
+          id="description"
+          name="description"
+          label="Group Description"
+          type="text"
+          value={newGroupData.description}
+          onChange={handleInputChange}
+          className="modal-input"
+        />
+        <DialogActions className="modal-actions">
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleCreateGroup} variant="contained" color="primary">Create</Button>
+        </DialogActions>
+      </Dialog>
       <h2 className="groups-text">GROUPS</h2>
       {groups.map((group, index) => (
         <div key={group.id} className="group">
           <div className='name-photos-container'>
-          <h2 className='group-name'>{group.name}</h2>
-          <button className='group-members' onClick={() => toggleMemberList(group)}>
-          <i class="bi bi-people"></i> ({group.members.length})
-          </button>
-          {group.showMembers && (
-            <ul>
-              {group.members.map((member) => (
-                <li key={member.id}>{member.username}</li>
-              ))}
-            </ul>
-          )}
+            <h2 className='group-name'>{group.name}</h2>
+            <p>Created by: {group.creatorUsername}</p>
+            <p>Created at: {new Date(group.created_at).toLocaleDateString()}</p>
+            <button className='group-members' onClick={() => toggleMemberList(group)}>
+              <i className="bi bi-people"></i> ({group.members.length})
+            </button>
+            {group.showMembers && (
+              <ul>
+                {group.members.map((member) => (
+                  <li key={member.id}>{member.username}</li>
+                ))}
+              </ul>
+            )}
           </div>
           <p className='group-description'>Description: {group.description}</p>
           <div className="group-posts">GROUP POSTS
