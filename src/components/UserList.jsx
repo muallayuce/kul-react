c4aae1085fd283dc156d9e381755b2e768ae9713
muallayuce <muallayuce@gmail.com>
2024-05-04 21:03:53 +0200

    search bar

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,6 +4,7 @@ import './UserList.css'; // Import CSS file for styling
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:8000/users')
@@ -12,11 +13,31 @@ const UserList = () => {
       .catch(error => console.error('Error fetching users:', error));
   }, []);
 
+  // Function to handle changes in the search input
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // Filter the list of users based on the search query
+  const filteredUsers = users.filter(user => {
+    return user.username.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
   return (
-    <div className="user-list-container"> 
+    <div className="user-list-container">
       <h1>User List</h1>
+      {/* Search bar */}
+      <div className="search-container">
+        <input 
+          type="text" 
+          placeholder="Search users..." 
+          value={searchQuery} 
+          onChange={handleSearchChange} 
+          className="search-input" 
+        />
+      </div>
       <ul className="user-list">
-        {users.map(user => (
+        {filteredUsers.map(user => (
           <li key={user.id} className="user-card">
             <Link to={`/profile/${user.id}`} className="user-link">
               {user.images && user.images.map(image => (
@@ -24,14 +45,14 @@ const UserList = () => {
               ))}
               <h3 className="username">{user.username}</h3>
             </Link>
-            <p className="email">Email: {user.email}</p> 
+            <p className="email">Email: {user.email}</p>
             <h4 className="posts-header">Posts:</h4>
             <ul className="posts-list">
               {user.posts.map((post, index) => (
                 <li key={index} className="post">{post.content}</li>
               ))}
             </ul>
-            <hr className="divider" /> 
+            <hr className="divider" />
           </li>
         ))}
       </ul>
