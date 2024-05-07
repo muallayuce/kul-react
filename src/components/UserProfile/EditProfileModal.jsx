b34f17d7f89795c6cab45573d2c7b993ce2b4ff2
muallayuce <muallayuce@gmail.com>
2024-05-07 10:31:42 +0200

    editprofile-CSS

--- src/components/UserProfile/EditProfileModal.jsx
+++ src/components/UserProfile/EditProfileModal.jsx
@@ -44,7 +44,7 @@ const EditProfileModal = ({ user, onCancel }) => {
     <Dialog open={true} onClose={onCancel}>
       <DialogTitle>Edit Profile</DialogTitle>
       <DialogContent>
-        <form onSubmit={handleSubmit}>
+        <form  id='edit-profile-form' onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username:</label>
             <input type="text" id="username" name="username" value={editedUser.username} onChange={handleChange} />
@@ -57,11 +57,11 @@ const EditProfileModal = ({ user, onCancel }) => {
             <label htmlFor="password">Password:</label>
             <input type="password" id="password" name="password" value={editedUser.password} onChange={handleChange} />
           </div>
-          <div className="button-group">
-            <Button type="submit">Save</Button>
-            <Button type="button" onClick={onCancel}>Cancel</Button>
-          </div>
         </form>
+        <div className="button-group">
+            <Button id='btn-grp' type="submit">Save</Button>
+            <Button id='btn-grp' type="button" onClick={onCancel}>Cancel</Button>
+          </div>
       </DialogContent>
     </Dialog>
   );
